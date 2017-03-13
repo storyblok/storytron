2a8c9fc9e7e503e5b35fcbf990eac31a6aa4a3c0
Dominik Angerer <dominikangerer1@gmail.com>
2017-03-13 11:26:19 +0100

    completly restructured storytron

---
+++ src/app.js
@@ -0,0 +1,5 @@
+// Here is the starting point for your application code.
+
+// Small helpers you might want to keep
+import './helpers/context_menu.js';
+import './helpers/external_links.js';
---
+++ src/menu/advanced_menu_template.js
@@ -0,0 +1,19 @@
+import { app, BrowserWindow } from 'electron';
+
+export const devMenuTemplate = {
+  label: 'Development',
+  submenu: [{
+    label: 'Reload',
+    accelerator: 'CmdOrCtrl+R',
+    click: () => {
+      BrowserWindow.getFocusedWindow().webContents.reloadIgnoringCache();
+    },
+  },
+  {
+    label: 'Quit',
+    accelerator: 'CmdOrCtrl+Q',
+    click: () => {
+      app.quit();
+    },
+  }],
+};
